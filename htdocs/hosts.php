c7a1978664ca0e1c86836afb63b5a750a69b3055
jakobspela <48317741+jakobspela@users.noreply.github.com>
2019-03-07 13:21:16 +0100

    Add files via upload

---
+++ htdocs/hosts.php
@@ -0,0 +1,12 @@
+<?php
+$servers = array(
+    'GAIA' => array(
+        'ip' => '192.168.1.12',
+        'purpose' => 'Webserver'
+    ),
+    'Other Server' => array(
+        'ip' => '192.168.23.68',
+        'purpose' => 'backup Server'
+    )
+);
+?>
